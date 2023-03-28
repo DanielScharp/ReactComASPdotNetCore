fe7833e980c376ccb3abadba61710fea31f9e4b7
Daniel Scharp <daniel.scharp@hotmail.com>
2023-03-28 17:27:28 -0300

    Initial commit

---
+++ AlunosApi/AlunosApi/Migrations/20230315161456_PopulaTabela.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlunosApi.Migrations
+{
+    public partial class PopulaTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Alunos",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 1, "mariapenha@yahoo.com", 23, "Maria da Penha" });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 2, "manuelbueno@yahoo.com", 22, "Manuel Bueno" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Alunos",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
---
+++ AlunosApi/AlunosApi/Services/AuthenticateService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace AlunosApi.Services
+{
+    public class AuthenticateService :IAuthenticate
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthenticateService(SignInManager<IdentityUser> signInManager) 
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                email, 
+                password, 
+                false, //Define se o cookie de entrada deve persistir depois que o navegador for fechado
+                lockoutOnFailure: false //Define que não quer que a conta do usuario seja bloqueada se a conexão falhar
+            );
+            return result.Succeeded;
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
